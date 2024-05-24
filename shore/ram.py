49b06bf37b7ced34c296e257410f03bf6add44f4
Schkimansky <ahmadchawla1432@gmail.com>
2024-05-24 22:11:47 +0500

    add function to get available ram

--- shore/ram.py
+++ shore/ram.py
@@ -5,3 +5,7 @@ from functools import lru_cache
 def get_ram():
     virtual_mem = _psutil.virtual_memory()    
     return virtual_mem.total
+
+def get_available_ram():
+    virtual_mem = _psutil.virtual_memory()    
+    return virtual_mem.available
