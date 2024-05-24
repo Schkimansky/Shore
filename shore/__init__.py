4e27fa6b4a69a716d7f0bb65779c57bb7a81e4a5
Schkimansky <ahmadchawla1432@gmail.com>
2024-05-24 22:20:02 +0500

    add function to get used swap ram

--- shore/__init__.py
+++ shore/__init__.py
@@ -1,6 +1,6 @@
 cpu = ['get_cpu_cores', 'get_virtual_cpu_cores', 'get_max_cpu_speed', 'get_min_cpu_speed', 'get_cpu_usage']
 ram = ['get_ram', 'get_available_ram', 'get_used_ram', 
-       'get_swap_ram']
+       'get_swap_ram', 'get_used_swap_ram']
 
 __all__ = cpu + ram
 
@@ -9,4 +9,4 @@ from ram import *
 
 _ = get_cpu_cores, get_virtual_cpu_cores, get_max_cpu_speed, get_min_cpu_speed, get_cpu_usage, \
     get_ram, get_available_ram, get_used_ram, \
-    get_swap_ram
+    get_swap_ram, get_used_swap_ram
