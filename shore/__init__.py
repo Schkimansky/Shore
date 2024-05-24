a667aeb659f627aae2afde8bae70eaa626a08cf0
Schkimansky <ahmadchawla1432@gmail.com>
2024-05-24 22:37:58 +0500

    add function to get file system type of disk partition

--- shore/__init__.py
+++ shore/__init__.py
@@ -2,7 +2,7 @@
 cpu  = ['get_cpu_cores', 'get_virtual_cpu_cores', 'get_max_cpu_speed', 'get_min_cpu_speed', 'get_cpu_usage']
 ram  = ['get_ram', 'get_available_ram', 'get_used_ram', 
         'get_swap_ram', 'get_used_swap_ram']
-disk = ['get_disk_partitions']
+disk = ['get_disk_partitions', 'get_file_system_type']
 
 __all__ = cpu + ram + disk
 
@@ -13,4 +13,4 @@ from disk import *
 _ = get_cpu_cores, get_virtual_cpu_cores, get_max_cpu_speed, get_min_cpu_speed, get_cpu_usage, \
     get_ram, get_available_ram, get_used_ram, \
     get_swap_ram, get_used_swap_ram, \
-    get_disk_partitions
+    get_disk_partitions, get_file_system_type
--- shore/disk.py
+++ shore/disk.py
@@ -3,3 +3,8 @@ import psutil as _psutil
 def get_disk_partitions():
     partitions = _psutil.disk_partitions()
     return [partition.device for partition in partitions]
+
+def get_file_system_type(partition_name):
+    partitions = _psutil.disk_partitions()
+    partition_dict = {partition.device : partition.fstype for partition in partitions}
+    return partition_dict[partition_name]
