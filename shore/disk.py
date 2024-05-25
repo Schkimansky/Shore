b07a3f65684782478d3498da1204561542362a24
Schkimansky <ahmadchawla1432@gmail.com>
2024-05-25 13:53:42 +0500

    add functino to get mount point of disk partition

--- shore/disk.py
+++ shore/disk.py
@@ -8,3 +8,8 @@ def get_file_system_type(partition_name):
     partitions = _psutil.disk_partitions()
     partition_dict = {partition.device : partition.fstype for partition in partitions}
     return partition_dict[partition_name]
+
+def get_mount_point(partition_name):
+    partitions = _psutil.disk_partitions()
+    partition_dict = {partition.device : partition.mountpoint for partition in partitions}
+    return partition_dict[partition_name]
