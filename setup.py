d7f5c7f4dcec73d24b30a6756be4fca74cdadd65
Schkimansky <ahmadchawla1432@gmail.com>
2024-05-25 14:45:42 +0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ here = os.path.abspath(os.path.dirname(__file__))
 with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
     long_description = "\n" + fh.read()
 
-VERSION = '1.0.0'
+VERSION = '1.0.1'
 DESCRIPTION = 'Get information about your system.'
 LONG_DESCRIPTION = 'This package allows you to get information about your system.'
 
@@ -21,7 +21,7 @@ setup(
     long_description_content_type="text/markdown",
     long_description=long_description,
     packages=find_packages(),
-    install_requires=[],
+    install_requires=['psutil'],
     keywords=['python', 'system', 'information', 'get information', 'get', 'get info', 'system info', 'get sys info' 'get system info'],
     classifiers=[
         "Development Status :: 5 - Production/Stable",
